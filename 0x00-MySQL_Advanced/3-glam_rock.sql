1c02a2dbc3fe46fa0a721a88895b736fde43ef4c
Nanakzy <nanakzy@gmail.com>
2024-07-11 17:31:37 +0000

    3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- 3-glam_rock.sql
+SELECT band_name, 
+       CASE
+           WHEN split IS NULL THEN 2022 - formed
+           ELSE split - formed
+       END AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
