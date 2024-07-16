b447b469907ca6e6378a0d0d044f3300888164df
Nanakzy <nanakzy@gmail.com>
2024-07-16 03:43:09 -0700

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
+"""change school topics"""
+
 
 def update_topics(mongo_collection, name, topics):
-    """Updates all topics of a school document based on the name"""
+    """Updates all topics of a school document based on the name
+    param: mongo collection,
+    param: name
+    param: topics
+    return:"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
