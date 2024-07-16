3d07538d3ffff4e05d00cd912fa50a3ab4860a30
Nanakzy <nanakzy@gmail.com>
2024-07-16 03:27:36 -0700

    correction

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,5 @@ from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection based on kwargs"""
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    new_documents = mongo_collection.insert_one(kwargs)
+    return new_documents.inserted_id
--- 0x01-NoSQL/9-main.py
+++ 0x01-NoSQL/9-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ 9-main """
 from pymongo import MongoClient
+
 list_all = __import__('8-all').list_all
 insert_school = __import__('9-insert_school').insert_school
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
