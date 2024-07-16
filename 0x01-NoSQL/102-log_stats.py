f7170ea99c3eb9d76ea4a82d4be8090f7d07ae60
Nanakzy <nanakzy@gmail.com>
2024-07-16 09:15:00 -0700

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""12-log_stats"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_check_count = collection.count_documents(
+            {"method": "GET", "path": "/status"}
+            )
+    print(f"{status_check_count} status check")
+
+    # Top 10 IPs
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ]
+    top_ips = list(collection.aggregate(pipeline))
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
