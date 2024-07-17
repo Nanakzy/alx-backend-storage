3038b3d86993eca01a9fa20391e15597d018fa89
Nanakzy <nanakzy@gmail.com>
2024-07-17 16:51:43 +0000

    task1 task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Cache class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -14,3 +14,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        data = self.get(key, lambda d: d.decode('utf-8'))
+        return data
+
+    def get_int(self, key: str) -> Optional[int]:
+        data = self.get(key, lambda d: int(d))
+        return data
